51c281545d63803a26b488090f3e4e7aaac95a4d
pleavinseven <pleavinseven@gmail.com>
2022-03-29 23:09:11 +0200

    - added keyboard layout

--- app/src/main/java/com/example/wordlesixcompose/ui/GameScreen.kt
+++ app/src/main/java/com/example/wordlesixcompose/ui/GameScreen.kt
@@ -1,15 +1,19 @@
 package com.example.wordlesixcompose.ui
 
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
@@ -27,16 +31,23 @@ import com.example.wordlesixcompose.ui.theme.WordleSixComposeTheme
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun HomeScreen(){
-        Column( modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(12.dp)
+fun HomeScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth().weight(1f)
         ) {
             WordGrid()
-            Keyboard()
-
         }
+        Column(modifier = Modifier
+            .fillMaxWidth().padding(0.dp, 4.dp),
+            verticalArrangement = Arrangement.Bottom) {
+            Keyboard()
+        }
+    }
 }
 
 
@@ -54,33 +65,98 @@ fun Title() {
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun WordGrid(){
-        LazyVerticalGrid(
-            cells = GridCells.Fixed(6),
-            modifier = Modifier,
-            state = rememberLazyListState(),
+fun WordGrid() {
+    LazyVerticalGrid(
+        cells = GridCells.Fixed(6),
+        modifier = Modifier,
+        state = rememberLazyListState(),
 
         ) {
-            items(30) { item ->
-                Card(
-                    modifier = Modifier.padding(4.dp, 8.dp)
-                        .aspectRatio(1f),
-                    backgroundColor = Color.White,
-                    border = BorderStroke(2.dp, Color.Black),
-                ) {
-                    Text(
-                        text = "",
-                        fontSize = 24.sp,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.padding(24.dp)
-                    )
-                }
+        items(30) { item ->
+            Card(
+                modifier = Modifier
+                    .padding(4.dp, 8.dp)
+                    .aspectRatio(1f),
+                backgroundColor = Color.White,
+                border = BorderStroke(2.dp, Color.Black),
+            ) {
+                Text(
+                    text = "",
+                    fontSize = 24.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(24.dp)
+                )
             }
         }
     }
+}
 
 
 @Composable
-fun Keyboard(){
-    // set keyboard here
+fun Keyboard() {
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+        MyKeyboardButton(text = "Q", 35)
+        MyKeyboardButton(text = "W", 35)
+        MyKeyboardButton(text = "E", 35)
+        MyKeyboardButton(text = "R", 35)
+        MyKeyboardButton(text = "T", 35)
+        MyKeyboardButton(text = "Y", 35)
+        MyKeyboardButton(text = "U", 35)
+        MyKeyboardButton(text = "I", 35)
+        MyKeyboardButton(text = "O", 35)
+        MyKeyboardButton(text = "P", 35)
+
+//        Button(onClick = { /*TODO*/ }, Modifier.weight(1F)) {
+//            Text(text = "Q", textAlign = TextAlign.Center)
+//        }
+
+    }
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+        MyKeyboardButton(text = "A", width = 40)
+        MyKeyboardButton(text = "S", width = 40)
+        MyKeyboardButton(text = "D", width = 40)
+        MyKeyboardButton(text = "F", width = 40)
+        MyKeyboardButton(text = "G", width = 40)
+        MyKeyboardButton(text = "H", width = 40)
+        MyKeyboardButton(text = "J", width = 40)
+        MyKeyboardButton(text = "K", width = 40)
+        MyKeyboardButton(text = "L", width = 40)
+    }
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+        MyKeyboardButton(text = "Enter", width = 70)
+        MyKeyboardButton(text = "Z", width = 35)
+        MyKeyboardButton(text = "X", width = 36)
+        MyKeyboardButton(text = "C", width = 36)
+        MyKeyboardButton(text = "V", width = 36)
+        MyKeyboardButton(text = "B", width = 36)
+        MyKeyboardButton(text = "N", width = 36)
+        MyKeyboardButton(text = "M", width = 36)
+//        IconButton(onClick = { /*TODO*/ }, modifier = Modifier.width(35.dp).background(Color.Blue)) {
+//            Icon(Icons.Filled.KeyboardArrowLeft, contentDescription = "Back")
+//        }
+        Button(onClick = { /*TODO*/ },
+            modifier = Modifier
+                .width(50.dp)
+                .height(60.dp)
+                .padding(0.dp, 2.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.LightGray)
+                ) {
+            Icon(
+                Icons.Filled.KeyboardArrowLeft,
+                contentDescription = "Back"
+            )
+        }
+    }
+}
+
+@Composable
+fun MyKeyboardButton(text: String, width: Int) {
+    Button(onClick = { /*TODO*/ },
+        modifier = Modifier
+            .width(width.dp)
+            .height(60.dp)
+            .padding(0.dp, 2.dp),
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color.LightGray)) {
+        Text(text = text, textAlign = TextAlign.Center)
+    }
 }
