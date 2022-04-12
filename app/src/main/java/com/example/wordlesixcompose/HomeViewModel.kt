347f4b09fffa3b0ae523b27d559e7af440a76d27
pleavinseven <pleavinseven@gmail.com>
2022-04-12 21:56:35 +0200

    - cards now coloured

--- app/src/main/java/com/example/wordlesixcompose/HomeViewModel.kt
+++ app/src/main/java/com/example/wordlesixcompose/HomeViewModel.kt
@@ -2,12 +2,18 @@ package com.example.wordlesixcompose
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.toMutableStateList
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
+
+data class CardData(val text: String, var colour: Color)
 
 class HomeViewModel : ViewModel() {
 
+
     var currentRow = 0
-    val guessArray = List(5) { List(6) { "" }.toMutableStateList() }
+
+    //    val guessArray = List(5) { List(6) { "" }.toMutableStateList() }
+    val guessArray = List(5) { List(6) { CardData("", Color.White) }.toMutableStateList() }
     var column = 0
     var rowChecked = false
     val word = "GUNMAN"
@@ -15,7 +21,7 @@ class HomeViewModel : ViewModel() {
 
     fun addLettersToGrid(text: String) {
         try {
-            guessArray[currentRow][column] = text
+            guessArray[currentRow][column] = CardData(text, Color.White)
             column += 1
         } catch (e: java.lang.IndexOutOfBoundsException) {
             if (currentRow == 4) {
@@ -25,7 +31,7 @@ class HomeViewModel : ViewModel() {
                     rowChecked = false
                     currentRow++
                     column = 0
-                    guessArray[currentRow][column] = text
+                    guessArray[currentRow][column] = CardData(text, Color.White)
                 }
             }
         }
@@ -35,19 +41,32 @@ class HomeViewModel : ViewModel() {
     fun checkLetterPlacementIsCorrect() {
         if (column == 6) {
             // remove letters form copies and check against each other to make sure yellow only called once
+            var yellowUsed = false
             val copyWord = word.map { it }.toMutableList()
             val copyGuess = guessArray[currentRow].map { it }.toMutableList()
             guessArray[currentRow].forEachIndexed { index, letter ->
-                if (letter[0] == word[index]) {
-                    println("green")
+                if (letter.text[0] == word[index]) {
+                    letter.colour = Color.Green
                     copyGuess.remove(letter)
-                    copyWord.remove(letter[0])
-                }
-            }
+                    copyWord.remove(letter.text[0])
 
-            for (item in copyWord) {
-                if (item.toString() in copyGuess) {
-                    println("yellow")
+
+                    // sort this ->
+
+
+                } else if (letter.text in word) {
+                    for (cardData in guessArray[currentRow]) {
+                        if (cardData.text == letter.text && cardData.colour == Color.Yellow) {
+                            yellowUsed = true
+                        }
+                        when (yellowUsed) {
+                            !yellowUsed -> letter.colour = Color.Yellow
+                            else -> letter.colour = Color.DarkGray
+                        }
+                        yellowUsed = false
+                    }
+                } else {
+                    letter.colour = Color.DarkGray
                 }
             }
         }
@@ -55,17 +74,15 @@ class HomeViewModel : ViewModel() {
         rowChecked = true
     }
 
-
-    private fun setColour(letter: String, colour: Int) { // 0 -> DarkGray, 1 -> Yellow, 2 -> Green
-
-    }
-
     fun removeLetter() {
         try {
-            guessArray[currentRow][column - 1] = ""
+            guessArray[currentRow][column - 1] = CardData("", Color.White)
             column--
         } catch (e: IndexOutOfBoundsException) {
 
         }
     }
 }
+
+// if letter was ever green, button is always green.
+// guess letters can be green or yellow?
